3eb23fec5a193f40dba59e6b763052ed27c6afcc
Steven Bouwkamp <bouwkast@mail.gvsu.edu>
2020-01-10 19:59:27 -0500

    Add necessary files

---
+++ src/loggist/LogProvider.cs.pp
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace YourRootNamespace
+{
+
+    internal class LogProvider<TCategoryName>
+    {
+        private ILogger<TCategoryName> _logger;
+
+        private ILogger<TCategoryName> GetCurrentClassLogger()
+        {
+            if (LoggingConfiguration.LoggerFactory == null || LoggingConfiguration.LoggerFactory.GetType() == typeof(NullLoggerFactory))
+            {
+                _logger = null;
+                return _logger;
+            }
+
+            if (_logger == null || _logger.GetType() == typeof(NullLogger<TCategoryName>) || _logger.GetType() == typeof(NullLogger))
+            {
+                _logger = LoggingConfiguration.LoggerFactory.CreateLogger<TCategoryName>();
+            }
+
+            return _logger;
+        }
+    }
+}
---
+++ src/loggist/LoggingConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace YourRootNamespace
+{
+    public static class LoggingConfiguration
+    {
+        /// <summary>
+        ///     Gets or sets the <see cref="ILoggerFactory"/> to power logging throughout this library.
+        /// </summary>
+        public static ILoggerFactory LoggerFactory { get; set; } = Microsoft.Extensions.Logging.Abstractions.NullLoggerFactory.Instance;
+    }
+}
