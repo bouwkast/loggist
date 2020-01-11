9972bc1446130adcc661112363a277d2626e9009
Steven Bouwkamp <bouwkast@mail.gvsu.edu>
2020-01-11 15:14:28 -0500

    Add basic class library example

---
+++ src/loggist.Example.Library/Foo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace loggist.Example.Library
+{
+    public class Foo
+    {
+        private ILogger<Foo> _logger = Logging.LogProvider<Foo>.GetCurrentClassLogger();
+
+
+        public void FooBar(string input)
+        {
+            using (_logger.BeginScope("Function Name {FunctionName} was called from class {ClassName}", nameof(FooBar), nameof(Foo)))
+            {
+                _logger.LogInformation("Input was {input}", input);
+            }
+        }
+    }
+}
